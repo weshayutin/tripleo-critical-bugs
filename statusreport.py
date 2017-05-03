6bf90f2fcbbba5c272e9001ac767243e2e4196b6
Wes Hayutin <weshayutin@gmail.com>
2017-05-03 09:11:49 -0400

    create a trello card in addition to email for escalation
    Do not rely on rel-del tool to scan for email and open a card.
    This will prevent duplicate emails from being sent when other
    scripts fail or are not running.
    
    Include the key "proa" in the subject of the escalation to
    indicate to other scripts not to create a card.

--- statusreport.py
+++ statusreport.py
@@ -92,10 +92,11 @@ class StatusReport(object):
         critical_bugs_with_out_escalation_cards = list(set(open_bugs) - set(match))
         return critical_bugs_with_out_escalation_cards
 
-    def send_email_to_create_escalation(self, config, critical_bugs_with_out_escalation_cards, list_of_bugs):
+    def create_escalation(self, config, critical_bugs_with_out_escalation_cards, list_of_bugs, trello_list):
         if not critical_bugs_with_out_escalation_cards:
             print "There are no bugs that require a new escalation"
         else:
+            # send email to list
             for bug in critical_bugs_with_out_escalation_cards:
                 bug_title = list_of_bugs[bug].title
                 bug = str(bug)
@@ -104,7 +105,7 @@ class StatusReport(object):
                 msg = MIMEMultipart()
                 msg['From'] = fromaddr
                 msg['To'] = toaddr
-                msg['Subject'] = "[CIX][LP:" + bug + "][tripleoci][unknown] " + bug_title
+                msg['Subject'] = "[CIX][LP:" + bug + "][tripleoci][proa] " + bug_title
                 body = "Automatically generated escalation via #tripleo alerts: https://bugs.launchpad.net/tripleo/+bug/" + bug
                 msg.attach(MIMEText(body, 'plain'))
                 text = msg.as_string()
@@ -113,6 +114,15 @@ class StatusReport(object):
                 server.sendmail(fromaddr, toaddr, text)
                 print "escalation email sent to " + toaddr + " subj:" + msg['Subject']
                 server.quit()
+
+                # create escalation card
+                trello_api_context = trello.ApiContext(config)
+                trello_cards = trello.Cards(trello_api_context)
+                trello_cards.create(msg['Subject'], trello_list)
+
+
+
+
 
 def main():
     arg_parser = argparse.ArgumentParser()
@@ -162,7 +172,7 @@ def main():
         critical_bugs_with_out_escalation_cards = report.compare_bugs_with_cards(bugs_with_alerts_open, cards_outtage )
         print "critical bugs not tracked on board " + str(critical_bugs_with_out_escalation_cards)
 
-        report.send_email_to_create_escalation(config, critical_bugs_with_out_escalation_cards, bugs_with_alerts_open)
+        report.create_escalation(config, critical_bugs_with_out_escalation_cards, bugs_with_alerts_open, trello_new_list_id)
 
 
 
