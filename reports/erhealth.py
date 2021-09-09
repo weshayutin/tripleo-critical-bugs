31631dcf06728cfa94a1fa8e34ecf477e70e6de5
frenzyfriday <anbanerj@redhat.com>
2021-09-09 19:16:36 +0200

    Add health url to card desc while create()
    Fixed more typos

--- reports/erhealth.py
+++ reports/erhealth.py
@@ -7,5 +7,5 @@ health_url = "http://health.sbarnea.com/"
 def get_health_link(bug_id):
     response = requests.get(base_url + str(bug_id) + ".yaml")
     if response.status_code == 200:
-        return health_url + str(bug_id)
+        return health_url + "#" + str(bug_id)
     return ""
